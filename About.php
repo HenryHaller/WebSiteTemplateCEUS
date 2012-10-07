23375047b1884bfde1841c45f07d6019928b3b99
StephanieCampbell <stephanie.campbell4@mail.mcgill.ca>
2012-10-06 20:10:25 -0400

    edited about page

--- About.php
+++ About.php
@@ -14,14 +14,14 @@
 		<?php include("Header.php");?>
 
 
-<div id="content">
+                <div id="content">
 			<div id="contentpage">
 				<div class="innertube">
 					<h1>About CEUS</h1>
 					<p>CEUS is your best friend.
-<p>Company Overview</p>
-<p>Your 2011-2012 CEUS Council:</p>
-<table cellpadding="5">
+                                        <p>Company Overview</p>
+                                        <p>Your 2011-2012 CEUS Council:</p>
+                                        <table cellpadding="5">
 						<tr>
 							<td><b>President</b></td>
 							<td><div>Neil DenToom</div></td>
@@ -63,51 +63,9 @@
 							<td>Ming Jia (Peter) Wang</td>
 						</tr>
 					</table>
-E-mail Address: ceus.mcgill@gmail.com
-Civil Lounge: Macdonald 291</p>
+                                    <p>E-mail Address: ceus.mcgill@gmail.com</p>
+                                    <p>Civil Lounge: Macdonald 291</p>
 
-<table cellpadding="5">
-						<tr>
-							<td><b>President</b></td>
-							<td><div>Neil DenToom</div></td>
-						</tr>
-						<tr>
-							<td><b>U1 Rep</b></td>
-							<td>Avril Levasseur</td>
-						</tr>
-                                                <tr>
-							<td><b>U2 Rep</b></td>
-							<td>Milad Moradi</td>
-						</tr>
-						<tr>
-							<td><b>U3</b></td>
-							<td>Martin DeSousa</td>
-						</tr>
-                                                <tr>
-							<td><b>VP Academic Affairs</b></td>
-							<td>Enis Hulli</td>
-						</tr>
-                                                <tr>
-							<td><b>VP Administration</b></td>
-							<td>Adam Hasham</td>
-						</tr>
-                                                <tr>
-							<td><b>VP Communications</b></td>
-							<td>Prashanth Selvakumar</td>
-						</tr>
-                                                <tr>
-							<td><b>VP Events</b></td>
-							<td>Patrick Lasserre</td>
-						</tr>
-                                                <tr>
-							<td><b>VP Finance</b></td>
-							<td>Carl Helou</td>
-						</tr>
-                                                <tr>
-							<td><b>VP Iternal</b></td>
-							<td>Ming Jia (Peter) Wang</td>
-						</tr>
-					</table>
 				</div>
 			</div>
 		</div>
